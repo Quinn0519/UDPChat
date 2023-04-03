a7cdbd358b05294da72ca21e3f089aa4c3f5a38a
Quinn0519 <1316407316@qq.com>
2023-04-03 20:42:40 +0800

    基本功能实现。

---
+++ Receive.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+using namespace std;
+
+int main()
+{
+	WSADATA ws;
+	WSAStartup(MAKEWORD(2, 2), &ws);
+
+	int server = socket(AF_INET, SOCK_DGRAM, 0);
+
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(8080);
+
+	int bindRet = bind(server, (sockaddr *)&addr, sizeof(addr));
+	if (bindRet == -1)
+	{
+		cout << "绑定端口失败，或许端口已经被占用" << errno << endl;
+		exit(0);
+	}
+
+	cout << "udp服务启动，正确8080端口监听" << endl;
+
+	// 来者是谁
+	sockaddr_in fromAddr;
+	int fromAddrLen = sizeof(fromAddr);
+
+	while (true)
+	{
+		// TODO：接收时间
+		char buf[1024] = {0};
+		/*
+		  int fd             : 套接字描述符
+		  char* buf          : 缓冲区
+		  int bufSize        : 缓冲区大小
+		  int flags          : 给0就可以了
+		  sockaddr* addr     : 用来保存接收到的地址
+		  int* addrLen       : 用来保存前一个参数的长度
+		  成功返回实际接收的字节数
+		  失败返回-1
+		*/
+		int recvRet = recvfrom(server, buf, sizeof(buf), 0, (sockaddr *)&fromAddr, &fromAddrLen);
+
+		if (recvRet == -1)
+		{
+			cout << "接收失败:请重新输入" << errno << endl;
+			break;
+		}
+
+		// IP端口号表示:127.0.0.1:8080
+		cout << inet_ntoa(addr.sin_addr) << ":" << ntohs(addr.sin_port) << "收到:" << buf << endl;
+	}
+
+	cout << "服务运行结束:" << errno << endl;
+}
---
+++ Send.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <time.h>
+#include <string>
+#include <thread>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+using namespace std;
+
+int main()
+{
+	WSADATA ws;
+	WSAStartup(MAKEWORD(2, 2), &ws);
+
+	int client = socket(AF_INET, SOCK_DGRAM, 0);
+
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); // TODO：域名解析 + IP 端口可设定
+	addr.sin_port = htons(8080);
+
+   
+	while (true)
+	{
+		// TODO：发送时间
+		// clock_t start, end;
+		// 	start = clock();
+		// 	end = clock(); 
+		
+		// 从这里开始
+		string str;
+		char *buf = "\0";
+		cout << "请输入：";
+		getline(cin, str);
+		buf = &str[0]; //******
+		cout << buf << endl;
+		// cout << "time = " << double(end - start) / CLOCKS_PER_SEC << "s" << endl;
+		int bufSize = strlen(buf);
+		// 到这里结束 重点学一学
+
+		int sendRet = sendto(client, buf, bufSize, 0, (sockaddr *)&addr, sizeof(addr));
+		if (sendRet == -1)
+		{
+			cout << "发送失败:" << errno << endl;
+			exit(0);
+		}
+	}
+}
+
+// TODO：美化终端显示
+// TODO：把他写的垃圾代码改成优秀代码，就是自己的了
+// TODOEND：都做好之后，移植Linux
