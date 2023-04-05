76eeca2be0cbb84167784c379d3cfc74882c6f5c
Quinn0519 <1316407316@qq.com>
2023-04-05 21:59:54 +0800

    可以自定义IP和端口，终端美化。

--- Receive.cpp
+++ Receive.cpp
@@ -13,10 +13,14 @@ int main()
 
 	int server = socket(AF_INET, SOCK_DGRAM, 0);
 
+	int ip_port;
+	cout << "请输入目标主机端口号：";
+	cin >> ip_port;
+
 	sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(8080);
+	addr.sin_port = htons(ip_port);
 
 	int bindRet = bind(server, (sockaddr *)&addr, sizeof(addr));
 	if (bindRet == -1)
@@ -45,7 +49,7 @@ int main()
 		  成功返回实际接收的字节数
 		  失败返回-1
 		*/
-		int recvRet = recvfrom(server, buf, sizeof(buf), 0, (sockaddr *)&fromAddr, &fromAddrLen);
+		int recvRet = recvfrom(server, buf, sizeof(buf), 0, (sockaddr *)&fromAddr, &fromAddrLen); // TODO：解决0.0.0.0
 		if (recvRet == -1)
 		{
 			cout << "接收失败:请重新输入" << errno << endl;
@@ -54,11 +58,12 @@ int main()
 
 		// 接收时间
 		time_t now = time(NULL);
-		char *date = ctime(&now);
+		string date = ctime(&now);
+		date.pop_back();
 
 		// IP端口号表示:127.0.0.1:8080
-		cout << "┌┅ Receive from: " << inet_ntoa(addr.sin_addr) << ":" << ntohs(addr.sin_port) << " ┅ "
-			 << date << "└┅ " << buf << endl
+		cout << "\033[36m┌┅「\033[0m \033[32mReceive from: " << inet_ntoa(addr.sin_addr) << ":" << ntohs(addr.sin_port) << "\033[0m \033[36m」┅「\033[0m \033[33m" << date << "\033[0m \033[36m」" << endl
+			 << "└┅\033[0m " << buf << endl
 			 << endl;
 	}
 
--- Send.cpp
+++ Send.cpp
@@ -14,23 +14,33 @@ int main()
 
 	int client = socket(AF_INET, SOCK_DGRAM, 0);
 
+	string ip_addrs;
+	int ip_port;
+	char *ip_addr = "\0";
+	cout << "请输入目标主机IP地址：";
+	cin >> ip_addrs;
+	ip_addr=&ip_addrs[0];
+	cout << "请输入目标主机端口号：";
+	cin >> ip_port;
+
 	sockaddr_in addr;
 	addr.sin_family = AF_INET;
-	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); // TODO：域名解析 + IP 端口可设定
-	addr.sin_port = htons(8080);
+	addr.sin_addr.S_un.S_addr = inet_addr(ip_addr); 
+	addr.sin_port = htons(ip_port);
 
 	while (true)
 	{
 		// 发送时间
 		time_t now = time(NULL);
-		char *date = ctime(&now);
+		string date = ctime(&now);
+		date.pop_back();
 
 		// 从这里开始
 		string str;
 		char *buf = "\0";
 
-		cout << "┌┅ Send To: " << inet_ntoa(addr.sin_addr) << ":" << ntohs(addr.sin_port) << " ┅ "
-			 << date << "└┅ ";
+		cout << "\033[36m┌┅「\033[0m \033[32mSend To: " << inet_ntoa(addr.sin_addr) << ":" << ntohs(addr.sin_port) << "\033[0m \033[36m」┅「\033[0m \033[33m" << date << "\033[0m \033[36m」" << endl
+			 << "└┅\033[0m ";
 		getline(cin, str);
 		buf = &str[0]; //******
 		cout << endl;
